a6b2efde58cdec085530a3df1630ec2ff961be7b
Joshua504 <chukwuebukajosh8@gmail.com>
2025-01-02 16:50:07 +0100

    first-commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+ <nav>
+  <div>
+    <router-link to="/"><img src="../assets/images/Logo.svg" alt=""></router-link>
+  </div>
+  <ul>
+    <router-link>
+      <li>home</li>
+    </router-link>
+    <router-link>
+      <li>about us</li>
+    </router-link>
+    <router-link>
+      <li>roadmap</li>
+    </router-link>
+    <router-link>
+      <li>tokenomics</li>
+    </router-link>
+    <router-link>
+      <li>join community</li>
+    </router-link>
+  </ul>
+  <div><button>presale</button></div>
+ </nav>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+<Navbar />
+</template>
+
+<script setup>
+import Navbar from "../components/Navbar.vue"
+
+</script>
+
+<style lang="scss">
+@use "../styles/main.scss" as *;
+
+
+
+</style>
